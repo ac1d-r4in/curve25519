8db092c7c3be2d2e2158bbe0238427d946e57ee6
ac1d-r4in <thelptvrus@gmail.com>
2024-11-18 20:47:35 +0300

    compile script

--- compile.sh
+++ compile.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-# Проверяем, передан ли файл в качестве аргумента
-if [ -z "$1" ]; then
-  echo "Usage: $0 filename.cpp"
-  exit 1
-fi
-
-# Получаем имя файла без расширения
-filename="${1%.*}"
-
-# Компилируем с указанием архитектуры arm64
-g++ -arch arm64 -o -lsodium "$filename" "$1"
-
-# Проверяем успешность компиляции
-if [ $? -eq 0 ]; then
-  ./"$filename"
-else
-  echo "Compilation failed."
-fi
+gcc curve25519.cpp -o curve25519 -I/opt/homebrew/include -L/opt/homebrew/lib -lsodium
+./curve25519 
